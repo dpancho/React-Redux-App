f75d21b378ade5189db2c2572b8746653b407879
Dale Pancho <dalepancho7@gmail.com>
2019-10-10 21:11:45 -0700

    MVP

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TeamPage from './components/teamPage'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MLB TEAMS</h1>
+      <TeamPage />
+    </div>
+  );
+}
+export default App;
